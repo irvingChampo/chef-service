1cf9b82ce836b4bac0af356ce6ad529733a8989b
Didiercito <231193@ids.upchiapas.edu.mx>
2025-12-10 04:21:13 -0600

    fff

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import { env } from './config/env';
 
-app.listen(env.port, () => {
-  console.log(`Chef Service cooking on port ${env.port}`);
+const port = Number(env.port) || 4009;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🍳 Chef Service cooking on port ${port}`);
 });
